a800b39c6dd4eb343b2124897d938b5da3647e45
Alex <skorohodov-ad@narod.ru>
2022-04-15 11:19:43 +0300

    Supported MacOS build

--- netlib/build.sh
+++ netlib/build.sh
@@ -1,14 +1,33 @@
-#!/usr/bin/sh
-
+#!/bin/sh
+echo "OS: $OSTYPE"
+generatorArg=" "
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        $generatorArg=" "
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+		$generatorArg=" -GXcode"
+        # Mac OSX
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        $generatorArg=" "
+elif [[ "$OSTYPE" == "msys" ]]; then
+        $generatorArg=" "
+elif [[ "$OSTYPE" == "win32" ]]; then
+        $generatorArg=" "
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        $generatorArg=" "
+else
+        $generatorArg=" "
+fi
 config="Debug"
-logArg="-DLOG_ON=ON"
+logArg=" -DLOG_ON=ON"
 build="Build-cmake"
-asioPath=' -DASIO_PATH="C:/lib/asio-1.22.1/include"'
+#asioPath=' -DASIO_PATH="C:/lib/asio-1.22.1/include"'
+asioPath=" -DASIO_PATH=/Users/skalexey/lib/asio-1.22.1/include"
+
+echo "Build directory: '$build'"
 
 [ ! -d "$build" ] && mkdir $build
 cd $build
-echo $build
 
-cmake ..$logArg$asioPath
+cmake ..$generatorArg$logArg$asioPath
 
 cmake --build . --config=$config
