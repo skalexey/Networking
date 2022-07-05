924e311c606ad73e85747552aa571684137fc8c3
skalexey <skorohodov-ad@narod.ru>
2022-07-05 14:44:59 +0300

    ASIO path added

--- build_config.sh
+++ build_config.sh
@@ -2,7 +2,7 @@
 
 buildFolderPrefix="Build"
 extraArg=" -DDEPS=${depsLocation}"
-extraArgWin=$extraArg
-extraArgMac=$extraArg
+extraArgWin="$extraArg -DASIO_PATH=C:/lib/asio-1.22.1/include"
+extraArgMac="$extraArg -DASIO_PATH=~/lib/asio-1.22.1/include"
 buildConfig="Debug"
 logArg=" -DLOG_ON=ON"
