7871ab2776b4bd90322380b048f0ca27c27f7d88
skalexey <skorohodov-ad@narod.ru>
2022-07-07 18:56:40 +0300

    Added comments

--- netlib/common/common.h
+++ netlib/common/common.h
@@ -7,6 +7,10 @@
 
 namespace anp
 {
-	typedef std::function<void(const std::vector<char>&, std::size_t, int)> on_client_data_cb;
+	typedef std::function<void(
+		const std::vector<char>&	// Buffer
+		, std::size_t				// Size
+		, int						// Id
+	)> on_client_data_cb;
 	typedef std::function<void(asio::error_code&) > asio_operation_cb;
 }
