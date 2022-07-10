1f90911a25b70cdb09c7e4f5082da4340ce9f082
skalexey <skorohodov-ad@narod.ru>
2022-07-10 19:37:01 +0300

    Add a new command to TCPServer

--- TCPServer/TCPServer.cpp
+++ TCPServer/TCPServer.cpp
@@ -46,6 +46,38 @@ int main()
 				s.stop();
 				break;
 			}
+			else if (msg == "send")
+			{
+				std::cout << "Message: ";
+				std::cin >> msg;
+				s.send(msg);
+				std::cout << "Sent '" << msg << "'\n";
+			}
+			else if (msg == "sf")
+			{
+				std::cout << "Send file input form...\n";
+				std::string m = 
+					"<!DOCTYPE html>"
+					"<html lang=\"en\">"
+					"<head>"
+					"<meta charset=\"utf-8\"/>"
+					"<title>upload</title>"
+					"</head>"
+					"<body>"
+					"<form action=\"http://127.0.0.1:8080\" method=\"post\" enctype=\"multipart/form-data\">"
+					"<p><input type=\"text\" name=\"text1\" value=\"text default\">"
+					"<p><input type=\"text\" name=\"text2\" value=\"a&#x03C9;b\">"
+					"<p><input type=\"file\" name=\"file1\">"
+					"<p><input type=\"file\" name=\"file2\">"
+					"<p><input type=\"file\" name=\"file3\">"
+					"<p><button type=\"submit\">Submit</button>"
+					"</form>"
+					"</body>"
+					"</html>"
+				;
+				s.send(m);
+				std::cout << "Sent: \n'" << m << "'\n";
+			}
 		}
 	}
 	catch (...)
