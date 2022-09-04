fc4a58bc8812f542fb6dc82e2a881b161648c688
Alex <alexeiskorokhodov@gmail.com>
2022-09-04 12:03:56 +0300

    Update deps configuration.

--- netlib/external_config.sh
+++ netlib/external_config.sh
@@ -6,7 +6,7 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $THIS_DIR/os.sh
 
 if is_windows; then
-	export netlib_asio_path="C:/lib/asio-1.22.1/include"
+	export netlib_libs="C:/lib"
 else
-	export netlib_asio_path="~/lib/asio-1.22.1/include"
+	export netlib_libs="~/lib"
 fi
