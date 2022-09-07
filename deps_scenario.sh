be9cb93fb43dd525d07accae035b3868d8c815e1
Alex <alexeiskorokhodov@gmail.com>
2022-09-07 11:40:07 +0300

    Use asio as a dependency. Support generalized dpendency download.

--- deps_scenario.sh
+++ deps_scenario.sh
@@ -6,6 +6,7 @@ function deps_scenario()
     source deps_config.sh
     
     download_dependency "Utils" "$depsLocation" "git@github.com:skalexey/Utils.git"
+    download_dependency "asio-1.22.1" "$depsLocation" "https://sourceforge.net/projects/asio/files/asio/1.22.1%20%28Stable%29/asio-1.22.1.tar.bz2/download"
 }
 
 deps_scenario $@
--- netlib/external_config.sh
+++ netlib/external_config.sh
@@ -4,9 +4,3 @@ export netlib_deps="${HOME}/Projects"
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $THIS_DIR/os.sh
-
-if is_windows; then
-	export netlib_libs="C:/lib"
-else
-	export netlib_libs="~/lib"
-fi
