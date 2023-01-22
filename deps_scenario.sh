60a37d769f119ab2967246470e95ccc54f55751d
skalexey <skorohodov-ad@narod.ru>
2023-01-22 22:25:02 +0100

    Auto update build scripts

--- deps_scenario.sh
+++ deps_scenario.sh
@@ -2,8 +2,9 @@
 
 function deps_scenario()
 {
-    source dependencies.sh
-    source deps_config.sh
+	local THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    source $THIS_DIR/dependencies.sh
+    source $THIS_DIR/deps_config.sh
     
     download_dependency "Utils" "$depsLocation" "git@github.com:skalexey/Utils.git"
     download_dependency "asio-1.22.1" "$depsLocation" "https://sourceforge.net/projects/asio/files/asio/1.22.1%20%28Stable%29/asio-1.22.1.tar.bz2/download"
