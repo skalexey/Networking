10e721ca94a3ea1cdd5592eff71ea821d7c08c4e
skalexey <skorohodov-ad@narod.ru>
2023-12-03 18:36:20 +0100

    Add DMBCore dependency to deps_scenario.sh.

--- HTTPClient/deps_scenario.sh
+++ HTTPClient/deps_scenario.sh
@@ -4,6 +4,8 @@ function deps_scenario()
 {
 	local THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     source $THIS_DIR/../deps_scenario.sh
+
+	download_dependency "DataModelBuilder" "$depsLocation" "git@github.com:skalexey/DataModelBuilder.git"
 }
 
 deps_scenario $@
