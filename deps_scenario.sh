4dffa8d34e4de1535161c72b2f912f180315ba03
skalexey <skorohodov-ad@narod.ru>
2024-01-04 13:25:26 +0100

    Consistency with the downloaded directory name.

--- deps_scenario.sh
+++ deps_scenario.sh
@@ -9,7 +9,7 @@ function deps_scenario()
     download_dependency "Utils" "$depsLocation" "git@github.com:skalexey/Utils.git"
     download_dependency "boost_1_81_0" "$depsLocation" "https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.bz2"
     download_dependency "certify" "$depsLocation" "https://github.com/djarek/certify.git"
-    download_dependency "OpenSSL-3.1.4" "$depsLocation" "https://www.openssl.org/source/openssl-3.1.4.tar.gz"
+    download_dependency "openssl-3.1.4" "$depsLocation" "https://www.openssl.org/source/openssl-3.1.4.tar.gz"
 }
 
 deps_scenario $@
